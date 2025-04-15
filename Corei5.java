9d21ddee6d8941a89b96a9b5e51472641e53ca7c
CCxbambix <luisalbertosalazar@ciencias.unam.mx>
2025-04-15 02:11:04 -0600

    Se hizo la parte de procesador, falta la fabrica

---
+++ Corei5.java
@@ -0,0 +1,99 @@
+public class Corei5 implements Procesador{
+    /**
+    * Atributos de la clase 
+    */
+    
+    /**
+     * Representa a la marca del procesador
+     */
+    private String marca;
+
+    /**
+     * Representa el modelo del procesador
+     */
+    private String modelo;
+
+    /**
+     * Representa los nucleos del procesador
+     */
+    private int nucleos;
+
+    /**
+     * Representa a los hilos del procesador
+     */
+    private int hilos;
+
+    /**
+     * Representa a la frecuencia del procesador
+     */
+    private double frecuencia;
+
+    /**
+     * Representa el precio del componente
+     */
+    private double precio;
+
+    /**
+     * Representa la descripcion del componente
+     */
+    private String descripcion;
+
+    /**
+    * Metodos de la clase 
+    */
+    
+    /**
+     * Regresa la marca del procesador
+     * @return String con valor de la marca del procesador
+     */
+    public String getMarca(){
+        return marca;
+    }
+
+    /**
+    * Regresa el modelo del procesador
+    * @return String con valor del modelo
+    */
+    public String getModelo(){
+        return modelo;
+    }
+
+
+    public int getNucleos(){
+        return nucleos;
+    }
+
+    public int getHilos(){
+        return hilos;
+    }
+
+    public double getFrecuencia(){
+        return frecuencia;
+    }
+    /**
+     * Regresa el precio de la SSD
+     * @return double con valor al precio del componente
+     */
+    public double getPrecio(){
+        return precio;
+    }
+
+    /**
+     * Regresa la descripcion del producto
+     * @return String con valor a la descripcion del componente
+     */
+    public String getDescripcion(){
+        return descripcion;
+    }
+
+    @Override
+    public void ensamblarSSD(){
+        modelo = "COre i5-13600K";
+    }
+
+    @Override
+    public void getTipo(){
+        System.out.pritnln(modelo);
+    }
+
+}
---
+++ Procesador.java
@@ -0,0 +1,13 @@
+public interface Procesador{
+    public String getMarca();
+    public String getModelo();
+    public String getNucleos();
+    public int getHilos();
+    public double getFrecuencia();
+    public double getPrecio();
+    public String getDescripcion();
+
+    public void ensamblarProcesador();
+
+    public void getTipo();
+}
