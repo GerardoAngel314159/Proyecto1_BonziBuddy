88fa951fc1cf92340b0601942c015974c015ea84
CCxbambix <luisalbertosalazar@ciencias.unam.mx>
2025-04-15 12:17:54 -0600

    Se creo la parte de motherboard

---
+++ Motherboard.java
@@ -0,0 +1,12 @@
+public interface Motherboard{
+
+    public String getMarca();
+    public String getModelo();
+    public String getChipset();
+    public String getSocket();
+    public String getFormato();
+    public double getPrecio();
+    public String getDescripcion();
+    public void ensamblarMotherboard();
+    public void getTipo();
+}
---
+++ ROGMaximusZ790Hero.java
@@ -0,0 +1,96 @@
+public class ROGMaximusZ790Hero implements Motherboard{
+
+    /**
+    * Atributos de la clase 
+    */
+    
+    /**
+    * Representa a la marca de la SSD
+    */
+    private String marca;
+
+    /**
+    * Representa el modelo de la SSD
+    */
+    private String modelo;
+
+    private String chipset;
+    /**
+    * Representa al socket de la Motherboard
+    */
+    private String socket;
+
+    /**
+    * Representa a el tipo de meoria que es la RAM
+    */
+    private String formato;
+    
+    /**
+    * Representa el precio del componente
+    */
+    private double precio;
+
+    /**
+    * Representa la descripcion del componente
+    */
+    private String descripcion;
+        /**
+    * Metodos de la clase 
+    */
+    
+    /**
+     * Regresa la marca de la HDD
+     * @return String con valor de la marca de la HDD
+     */
+    public String getMarca(){
+        return marca;
+    }
+
+    /**
+    * Regresa el modelo de la HDD
+    * @return String con valor del modelo
+    */
+    public String getModelo(){
+        return modelo;
+    }
+
+
+    public String getChipset(){
+        return chipset;
+    }
+
+    public String getSocket(){
+        return socket;
+    }
+
+    public String getformato(){
+        return formato;
+    }
+
+    /**
+     * Regresa el precio de la HDD
+     * @return double con valor al precio del componente
+     */
+    public double getPrecio(){
+        return precio;
+    }
+
+    /**
+     * Regresa la descripcion del producto
+     * @return String con valor a la descripcion del componente
+     */
+    public String getDescripcion(){
+        return descripcion;
+    }
+
+    @Override
+    public void ensamblarMotherboard(){
+        modelo = "ROGMaximusZ790Hero"
+    }
+
+    @Override
+    public void getTipo(){
+        System.out.pritnln(modelo);
+    }
+
+} 
