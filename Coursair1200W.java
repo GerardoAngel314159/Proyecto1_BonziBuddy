9cb4cfc9fe1579a77bf7ad6369a3984005fda108
NatzuF <GabrielCA_19@ciencias.unam.mx>
2025-04-17 17:39:36 -0600

    Agrego las clases de las Fuentes de Alimentacion

---
+++ Coursair1200W.java
@@ -0,0 +1,85 @@
+/**
+ * Clase que representa una fuente de poder Coursair de 1200W.
+ * Contiene información basica sobre el producto.
+ * el precio y la certificacion de la unidad.
+ */
+public class Coursair1200W {
+    /**
+     * Marca de la fuente de poder.
+     */
+    private String marca;
+
+    /**
+     * Modelo específico de la fuente de poder.
+     */
+    private String modelo;
+
+    /**
+     * Potencia maxima de salida en vatios (W).
+     */
+    private int potencia;
+
+    /**
+     * Precio de la fuente de poder.
+     */
+    private double precio;
+
+    /**
+     * Certificacion de eficiencia energetica.
+     */
+    private String certificacion;
+
+    /**
+     * Devuelve la marca de la fuente de poder.
+     *
+     * @return la marca.
+     */
+    public String getMarca() {
+        return marca;
+    }
+
+    /**
+     * Devuelve el modelo de la fuente de poder.
+     *
+     * @return el modelo.
+     */
+    public String getModelo() {
+        return modelo;
+    }
+
+    /**
+     * Devuelve la potencia nominal de la fuente de poder.
+     *
+     * @return potencia en vatios.
+     */
+    public int getPotencia() {
+        return potencia;
+    }
+
+    /**
+     * Devuelve el precio de la fuente de poder.
+     *
+     * @return precio de la Fuente de Alimentacion.
+     */
+    public double getPrecio() {
+        return precio;
+    }
+
+    /**
+     * Devuelve la certificacion energetica de la fuente de poder.
+     *
+     * @return certificacion.
+     */
+    public String getCertificacion() {
+        return certificacion;
+    }
+
+    /**
+     * Metodo que simula el proceso de ensamblaje de la fuente de poder.
+     */
+    public void ensamblarFA() {
+    }
+}
+
+
+
---
+++ FuenteAlimentacion.java
@@ -0,0 +1,7 @@
+public interface FuenteAlimentacion {
+    public String getMarca();
+    public String getModelo();
+    public int getPotencia();
+    public String certificacion();
+    public void ensamblarFA();
+}
