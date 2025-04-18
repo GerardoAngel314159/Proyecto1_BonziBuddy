70a9f52ce98ff08b4231dd4310a4a7d784ed3c12
NatzuF <GabrielCA_19@ciencias.unam.mx>
2025-04-18 16:31:24 -0600

    implemento las cosas para Adapter

---
+++ AMDRX9700.java
@@ -0,0 +1,66 @@
+/**
+ * Representa una tarjeta grafica AMDRX9700.
+ */
+public class AMDRX9700 {
+    /**
+     * Marca del fabricante de la tarjeta grafica.
+     */
+    private String marca;
+
+    /**
+     * Modelo especifico de la tarjeta grafica.
+     */
+    private String modelo;
+
+    /**
+     * Cantidad de memoria RAM dedicada a la tarjeta.
+     */
+    private int ramTG;
+
+    /**
+     * Precio de la tarjeta grafica en la moneda local.
+     */
+    private double precio;
+
+    /**
+     * Obtiene la marca de la tarjeta grafica.
+     *
+     * @return la marca
+     */
+    public String getMarca() {
+        return marca;
+    }
+
+    /**
+     * Obtiene el modelo de la tarjeta grafica.
+     *
+     * @return el modelo especifico
+     */
+    public String getModelo() {
+        return modelo;
+    }
+
+    /**
+     * Obtiene la cantidad de memoria RAM de la tarjeta.
+     *
+     * @return la memoria RAM de la Tarjeta Grafica
+     */
+    public int getRamTG() {
+        return ramTG;
+    }
+
+    /**
+     * Obtiene el precio de la tarjeta grafica.
+     *
+     * @return el precio en la moneda local
+     */
+    public double getPrecio() {
+        return precio;
+    }
+
+    /**
+     * Metodos para ensamblar la tarjeta gráfica.
+     */
+    public void ensamblarTargeta() {
+    }
+}
---
+++ Ryzen38300G.java
@@ -0,0 +1,108 @@
+/**
+ * Representa un procesador Ryzen38300G.
+ */
+public class Ryzen38300G {
+    /**
+     * Marca del procesador.
+     */
+    public String marca;
+
+    /**
+     * Modelo especifico del procesador.
+     */
+    public String modelo;
+
+    /**
+     * Numero de nucleos del procesador.
+     */
+    public int nucleos;
+
+    /**
+     * Numero de hilos del procesador.
+     */
+    public int hilos;
+
+    /**
+     * Frecuencia de operacion en GHz.
+     */
+    public double frecuencia;
+
+    /**
+     * Precio del procesador en la moneda local.
+     */
+    public double precio;
+
+    /**
+     * Descripcion adicional del procesador.
+     */
+    public String descripcion;
+
+    /**
+     * Obtiene la marca del procesador.
+     *
+     * @return la marca del procesador
+     */
+    public String getMarca() {
+        return marca;
+    }
+
+    /**
+     * Obtiene el modelo del procesador.
+     *
+     * @return el modelo especifico
+     */
+    public String getModelo() {
+        return modelo;
+    }
+
+    /**
+     * Obtiene el numero de nucleos.
+     *
+     * @return la cantidad de nucleos
+     */
+    public int getNucleos() {
+        return nucleos;
+    }
+
+    /**
+     * Obtiene el numero de hilos.
+     *
+     * @return la cantidad de hilos
+     */
+    public int getHilos() {
+        return hilos;
+    }
+
+    /**
+     * Obtiene la frecuencia de operacion.
+     *
+     * @return la frecuencia en GHz
+     */
+    public double getFrecuencia() {
+        return frecuencia;
+    }
+
+    /**
+     * Obtiene el precio del procesador.
+     *
+     * @return el precio
+     */
+    public double getPrecio() {
+        return precio;
+    }
+
+    /**
+     * Obtiene la descripcion del procesador.
+     *
+     * @return la descripcion adicional
+     */
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    /**
+     * Realiza el ensamblaje del procesador
+     */
+    public void ensamblarProcesador() {
+    }
+}
